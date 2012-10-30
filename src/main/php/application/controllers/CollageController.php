0bb00043df14ede46ddf3b4bd8f915fe29ddfd1c
Garland Kan <garlandk@gmail.com>
2012-10-30 17:21:30 -0400

    latest changes

--- src/main/php/application/controllers/CollageController.php
+++ src/main/php/application/controllers/CollageController.php
@@ -18,11 +18,16 @@ class CollageController extends Zend_Controller_Action
     }
     public function preDispatch(){
 
-        // If user dont have a session forward them to the home page
-        $user_id = $this->facebook->getUser();
-    
-        if($user_id == 0)
+        try{
+            $me = $this->facebook->api('/me');
+
+            if($me){
+                // User is logged in already
+            }
+        }catch(FacebookApiException $e){
+            // User is not logged in
             $this->_helper->redirector('index', 'index');
+        }
     }
     public function __call($method, $args)
     {
@@ -34,65 +39,62 @@ class CollageController extends Zend_Controller_Action
     }
     public function photosAction()
     {
-//        $access_token = $facebook->getAccessToken();
-        //$facebook->setAccessToken($access_token);
-//        echo $access_token.'<br/>';
-
-//        $user_id = $facebook->getUser();
-//        echo $user_id.'<br/>';
-
         $response = $this->facebook->api('/me/photos');        
-//     print_r($response);
         $this->view->mePhotos = $response;
     }
     public function generateAction(){
 
         $selectedPics = $this->_request->getParam( 'pic' );
 
-        // call some stuff from algorithms.io to create the collage
-        include_once('AlgorithmsIO/classes/Utilities.php');
-        $utilities = new Utilities();
+        if(count($selectedPics) > 0 ){
 
-        // Make call to get HP auth
-        $url = 'http://pod1.staging.v1.api.algorithms.io/jobs';
-        $headers = array('authToken: c1a77f12caa5b03ee5654838f1741be0');
-        $data['job']['algorithm']['id'] = "33";
-        $data['job']['algorithm']['params']['hp_username'] = 'garland';
-        $data['job']['algorithm']['params']['hp_password'] = 'teachMe!';
-        $data['job']['algorithm']['params']['hp_tenantID'] = '41738351831371';
-        $data['job']['algorithm']['params']['params'] = array();
-        $data['job']['outputType'] = 'json';
-        $data['job']['method'] = 'sync';
-        $data['job']['datasources'] = array();
-        $post_params['job_params'] = json_encode( $data );
+            // call some stuff from algorithms.io to create the collage
+            include_once('AlgorithmsIO/classes/Utilities.php');
+            $utilities = new Utilities();
 
-        $response = $utilities->curlPost($url, $post_params, $headers);
-        $responseArray = json_decode($response, true);
-//echo $response;
-        if(json_last_error() == 0){
-
-            // Make call to generate collage 
-            unset( $data['job']['algorithm']['params'] );
-            unset( $post_params );
-
-            $data['job']['algorithm']['id'] = "34";
-            $data['job']['algorithm']['params']['hp_authToken'] = $responseArray['hp_authToken'];
-            $data['job']['algorithm']['params']['hp_template_url'] = 'https://region-a.geo-1.objects.hpcloudsvc.com:443/v1/41738351831371/collage-templates/template-heart.png';
-            $data['job']['algorithm']['params']['params'] = $selectedPics;
+            // Make call to get HP auth
+            $url = 'http://pod1.staging.v1.api.algorithms.io/jobs';
+            $headers = array('authToken: c1a77f12caa5b03ee5654838f1741be0');
+            $data['job']['algorithm']['id'] = "33";
+            $data['job']['algorithm']['params']['hp_username'] = 'garland';
+            $data['job']['algorithm']['params']['hp_password'] = 'teachMe!';
+            $data['job']['algorithm']['params']['hp_tenantID'] = '41738351831371';
+            $data['job']['algorithm']['params']['params'] = array();
+            $data['job']['outputType'] = 'json';
+            $data['job']['method'] = 'sync';
+            $data['job']['datasources'] = array();
             $post_params['job_params'] = json_encode( $data );
 
             $response = $utilities->curlPost($url, $post_params, $headers);
             $responseArray = json_decode($response, true);
-
+            //echo $response;
             if(json_last_error() == 0){
-//print_r($responseArray);
-                // Set the colloage png to the view
-                $this->view->collagePicture = $responseArray['collage']['id_pic'];
+
+                // Make call to generate collage 
+                unset( $data['job']['algorithm']['params'] );
+                unset( $post_params );
+
+                $data['job']['algorithm']['id'] = "34";
+                $data['job']['algorithm']['params']['hp_authToken'] = $responseArray['hp_authToken'];
+                $data['job']['algorithm']['params']['hp_template_url'] = 'https://region-a.geo-1.objects.hpcloudsvc.com:443/v1/41738351831371/collage-templates/template-heart.png';
+                $data['job']['algorithm']['params']['params'] = $selectedPics;
+                $post_params['job_params'] = json_encode( $data );
+
+                $response = $utilities->curlPost($url, $post_params, $headers);
+                $responseArray = json_decode($response, true);
+
+                if(json_last_error() == 0){
+                    //print_r($responseArray);
+                    // Set the colloage png to the view
+                    $this->view->collagePicture = $responseArray['collage']['id_pic'];
+                }
+
+            }else{
+                return '{"result":"error response was not a valid json"}';
             }
-
         }else{
-            return '{"result":"error response was not a valid json"}';
+            // Set a default pic
+            $this->view->collagePicture = 'b2a5f67bfb84f961c487d06783885c6b92a24fa3';
         }
-
     }
 }
